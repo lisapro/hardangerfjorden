6151330a29a73ff22ff42166f816caa436f8a349
lisapro <elisaveta.pro@gmail.com>
2018-01-26 15:48:41 +0100

    functions to plot dots, code cleaning

--- twomap.py
+++ twomap.py
@@ -14,26 +14,11 @@ gs2 = gridspec.GridSpec(1,1)
 gs2.update(left = 0.65,right = 0.95,bottom = 0.3, top = 0.99) 
 ax2 = fig.add_subplot(gs2[0])
 
-#map = Basemap(projection='stere', 
-#              lat_0= 5, lon_0 =5)
-
-
 map = Basemap(#width=150000,height=990000,
             resolution='l',projection='stere',\
             lat_0=70,lon_0=14,
             llcrnrlon = 5 ,llcrnrlat = 54 ,
             urcrnrlon = 27 ,urcrnrlat =70,ax = ax )
-
-
-#axins = inset_axes(ax2,
-#                        #width="30%", # width = 30% of parent_bbox
-#                        height="30%", # height : 1 inch
-#                        loc=9)
-
-'''map2 = Basemap(projection='merc',
-               llcrnrlat=59,urcrnrlat=63,
-            llcrnrlon=5,urcrnrlon=10,
-            resolution='i',ax = ax2)'''
 
 map2 = Basemap(projection='merc',llcrnrlat = 59.9,
              urcrnrlat=60.2,
@@ -58,53 +43,13 @@ meridians = [5,10,15,20]
 
 parallels2 = [59.9,60,60.1,60.2]
 meridians2 = [5.4,5.6,5.8,6]
+
 map.drawparallels(parallels, labels = [1,1,0,0]) # draw parallels
 map.drawmeridians(meridians, labels = [0,0,1,1]) # draw parallels
 
 map2.drawparallels(parallels2, labels = [1,1,1,1]) # draw parallels
 map2.drawmeridians(meridians2, labels = [1,1,1,1]) # draw parallels
 
-#m1.drawparallels([60.1,60.15], labels = [0,1,1,1]) # draw parallels
-
-
-#axins = zoomed_inset_axes(ax, 7, loc=1)
-'''
-axins = inset_axes(ax,
-                        width="30%", # width = 30% of parent_bbox
-                        height="30%", # height : 1 inch
-                        loc=9)
-x,y = map(5.5,60.0) # we define the corner 1
-x2,y2 = map(6.5,61.5) # then corner 2
-
-axins.set_xlim(x,x2) # and we apply the limits of the zoom plot to the inset axes
-axins.set_ylim(y,y2) # idem
-
-#assert loc in range(1, 11)  # called only internally
-
-#axins.set_xlim(50, 6)
-axins.set_ylim(60,65)
-
-plt.xticks(visible=False)
-plt.yticks(visible=False)
-
-map2 = Basemap(llcrnrlon=5,llcrnrlat=60,
-               urcrnrlon=6,urcrnrlat=61, 
-               ax=axins,resolution='h')
-
-map2.drawmapboundary(fill_color='#7777ff')
-map2.fillcontinents(color='#ddaa66', lake_color='#7777ff', zorder=0)
-map2.drawcoastlines()
-map2.drawcountries()
-
-#map2.scatter(x, y, s=cases/5., c='r', alpha=0.5)
-mark_inset(ax, axins, loc1=1, loc2=3, fc="none", ec="0.5")
-
-#mark_inset(ax, axins, loc1=10, loc2=15, fc="none", ec="0.5")
-'''
-
-
-
-#mark_inset(ax, ax2, loc1=2, loc2=3, fc="none", ec="0.5")
 
 lon = [5.861 , 5.7815, 5.769, 5.959, 5.928, 5.925] 
 lat = [59.97478, 59.97821, 59.9757, 60.0471, 60.1239, 60.1212]
@@ -119,7 +64,6 @@ name_sed2 = ['200']
 
 lon7 = 5.930716667
 lat7 = 60.12692
-name = ['st1 ','st2','st3','st4','st5','st6']
 
 x_farm1 = 5.772001
 y_farm1 = 59.977903
@@ -137,89 +81,67 @@ y_farm4 = 60.121650
 x_farm5 = 5.669370  
 y_farm5 = 59.954219
 
+
 x1,y1 = map2(lon[0],lat[0])
 x2,y2 = map2(lon[1],lat[1])
 x3,y3 = map2(lon[2],lat[2])
 x4,y4 = map2(lon[3],lat[3])
 x5,y5 = map2(lon[4],lat[4])
 x6,y6 = map2(lon[5],lat[5])
-
 x7,y7 = map2(lon7,lat7)
 
 x7_1,y7_1 = map(lon7,lat7)
 
-
 x_sed1,y_sed1 = map2(lon_sed,lat_sed)
 x_sed2,y_sed2 = map2(lon_sed2,lat_sed2)
 
-map2.scatter(lon_sed, lat_sed,latlon= True , color ='#880303',  label = "sediment station", 
-          edgecolor = '#49361f', s= 55, zorder = 10)
-map2.scatter(lon_sed2, lat_sed2,latlon= True , color ='#880303',
-          edgecolor = '#49361f', s= 55, zorder = 10)
 
+def plot_m(xf,yf,mapf,size,color,edgecolor,label = None, marker = None):
 
-map2.scatter(lon7, lat7,latlon= True , color ='#ff7e37',
-          edgecolor = '#995f3f', s= 55, zorder = 10)
+    mapf.scatter(xf,yf,latlon = True, marker = marker,
+                color = color, s = size , edgecolor = edgecolor,
+                label = label, zorder = 10 ,alpha = 1)
 
+starsize = 105  
+s_ecolor = '#235063' 
+star_ecolor = '#f29f10' #'k' ##824c02
+dot_size = 55
+farmcolor = '#f6e32c'  
+sed_color ='#880303'
+wat_color = '#007cb2'
 
+plot_m(lon_sed, lat_sed,map2,dot_size,sed_color,s_ecolor,"sediment station")
+plot_m(lon_sed2, lat_sed2,map2,dot_size,sed_color,s_ecolor)
 
+plot_m(lon7, lat7,map2,dot_size,wat_color,s_ecolor)
+plot_m(lon, lat,map2,dot_size,wat_color,s_ecolor,"water column station")
 
-map2.scatter(lon, lat,latlon= True , color ='#ff7e37',
-          edgecolor = '#995f3f', s= 55, zorder = 10, label = "water column station")
+plot_m(lon7,lat7,map,550,farmcolor,s_ecolor,None,"*")   
 
-map2.scatter(x_farm1,y_farm1,latlon = True, marker = "*", 
-          color ='#f6e32c', edgecolor = '#b8894e',
-          label = 'Fish farm',s = 85, zorder = 10 ,alpha = 1)
-
-map2.scatter(x_farm2,y_farm2,latlon = True, marker = "*", color ='#f6e32c',
-           s = 85, edgecolor = '#b8894e', zorder = 10 ,alpha = 1)
-map2.scatter(x_farm3,y_farm3,latlon = True, marker = "*", color ='#f6e32c',
-           s = 85, edgecolor = '#b8894e', zorder = 10 ,alpha = 1)
-map2.scatter(x_farm4,y_farm4,latlon = True, marker = "*", color ='#f6e32c',
-           s = 85, edgecolor = '#b8894e', zorder = 10 ,alpha = 1)
-map2.scatter(x_farm5,y_farm5,latlon = True, marker = "*", color ='#f6e32c',
-           s = 85, edgecolor = '#b8894e', zorder = 10 ,alpha = 1)
-
-
-
-map.scatter(lon7, lat7,latlon= True , 
-            color ='#ff7e37',
-          edgecolor = '#995f3f', s= 55,
-          zorder = 10, label = "water profiles")
+plot_m(x_farm1,y_farm1,map2,starsize,farmcolor,star_ecolor,'Fish farm',"*")       
+plot_m(x_farm2,y_farm2,map2,starsize,farmcolor,star_ecolor,None,"*") 
+plot_m(x_farm3,y_farm3,map2,starsize,farmcolor,star_ecolor,None,"*")
+plot_m(x_farm4,y_farm4,map2,starsize,farmcolor,star_ecolor,None,"*")
+plot_m(x_farm5,y_farm5,map2,starsize,farmcolor,star_ecolor,None,"*")  
 
 txtsize = 10
-st = 0
-for xy in ((x1,y1), (x2,y2)) :
-    ax2.annotate(name[st], xy,xytext=(7, -19), size= txtsize, 
-                arrowprops=dict(arrowstyle="->",connectionstyle="arc3"),
-                textcoords='offset points')
-    st += 1
-    
-ax2.annotate('st3 ', (x3,y3), xytext=(-3, -19),size= txtsize,
+
+def to_annotate(title,coords,txt_coords,axis):  
+    axis.annotate(title, coords, xytext = txt_coords,size= txtsize,
             arrowprops=dict(arrowstyle="->",connectionstyle="arc3"),
             textcoords='offset points')
-ax2.annotate('st4 ', (x4,y4), xytext=(1, -19),size= txtsize,
-            arrowprops=dict(arrowstyle="->",connectionstyle="arc3"),
-             textcoords='offset points')
-ax2.annotate('st5 ', (x5,y5), xytext=(12, 0),size= txtsize,
-            arrowprops=dict(arrowstyle="->",connectionstyle="arc3"),
-             textcoords='offset points')
-ax2.annotate('st199 ', (x_sed1,y_sed1), xytext=(12, -15),size= txtsize,
-            arrowprops=dict(arrowstyle="->",connectionstyle="arc3"),
-             textcoords='offset points')
 
-ax2.annotate('st200 ', (x_sed2,y_sed2), xytext=(12, -25),size= txtsize,
-            arrowprops=dict(arrowstyle="->",connectionstyle="arc3"),
-             textcoords='offset points')
+to_annotate('st1', (x1,y1),(7, -19),ax2) 
+to_annotate('st2', (x2,y2),(7, -19),ax2) 
+to_annotate('st3', (x3,y3),(-3, -19),ax2) 
+to_annotate('st4', (x4,y4),(1, -19),ax2) 
+to_annotate('st5', (x5,y5),(12, 0),ax2) 
+to_annotate('st6', (x6,y6),(12, -12),ax2) 
+to_annotate('st7', (x7,y7),(12, 15),ax2) 
 
-ax2.annotate('st6 ', (x6,y6), xytext=(12,-12),size= txtsize,
-            arrowprops=dict(arrowstyle="->",connectionstyle="arc3"),
-             textcoords='offset points')   
-
- 
-ax2.annotate('st7', (x7,y7), xytext=(12,15),size= txtsize,
-            arrowprops=dict(arrowstyle="-",connectionstyle="arc3"),
-             textcoords='offset points')      
+to_annotate('st199', (x_sed1,y_sed1),(12, -15),ax2) 
+to_annotate('st200', (x_sed2,y_sed2),(12, -25),ax2) 
+      
 '''    
 from matplotlib.patches import ConnectionPatch
 con = ConnectionPatch(xyA=(x_patch,y_patch), xyB=(x7_1,y7_1),
